15f405a07e9867b07f25209159c72dc3c4f627c9
unknown <nkinbox@outlook.com>
2019-10-31 14:12:17 +0530

    Call Logger

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 
 Route::middleware('AfterWork')->post('/insert', 'APIController@insert');
 
+Route::post('logger/register', 'CallRecorderController@registerAgent');
 Route::post('logger/login', 'CallRecorderController@login');
 Route::post('logger/sim', 'CallRecorderController@sim_allocation');
 Route::post('logger/create', 'CallRecorderController@createLog');
