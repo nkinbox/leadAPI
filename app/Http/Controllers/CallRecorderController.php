9db810d13af41b95ecc78e39958884cb40b99488
unknown <nkinbox@outlook.com>
2019-11-02 20:53:36 +0530

    API Reduced

--- app/Http/Controllers/CallRecorderController.php
+++ app/Http/Controllers/CallRecorderController.php
@@ -14,19 +14,46 @@ class CallRecorderController extends Controller {
     }
     public function login(Request $request) {
         $this->validate($request, [
+            'name' => 'required|string|max:50',
             'user_name' => 'required|max:50|exists:agents',
             'password' => 'required|min:6',
+            'sim_allocation' => 'required|array',
+            'sim_allocation.*.sim_id' => 'required|digits_between:10,50',
+            'sim_allocation.*.operator' => 'required|string|max:30',
+            'sim_allocation.*.dial_code' => 'nullable|string|max:5',
+            'sim_allocation.*.phone_number' => 'nullable|string|max:15',
         ]);
         $agent = Agents::where('user_name', $request->user_name)->first();
-        if (Hash::check($request->password, $agent->password)) {
-            $agent->api_token = sha1($request->password.$request->user_name.time());
-            $agent->save();
-            $this->response['api_token'] = $agent->api_token;
-            return response()->json($this->response, 200);
+        if($agent) {
+            if(!Hash::check($request->password, $agent->password)) {
+                $this->response['success'] = 0;
+                return response()->json($this->response, 422);
+            }
         } else {
-            $this->response['message'] = 'username or password does not match';
-            return response()->json($this->response, 422);
+            $agent = new Agents;
+            $agent->name = $request->name;
+            $agent->user_name = $request->user_name;
+            $agent->password = Hash::make($request->password);
+            $agent->save();
         }
+        foreach($request->sim_allocation as $sim_allocation) {
+            $sim = SimAllocation::find($sim_allocation['sim_id']);
+            if($sim) {
+                $sim->operator = $sim_allocation['operator'];
+                $sim->agent_id = $agent->id;
+                $sim->save();
+            } else {
+                $sim = new SimAllocation;
+                $sim->id = $sim_allocation['sim_id'];
+                $sim->operator = $sim_allocation['operator'];
+                $sim->dial_code = isset($sim_allocation['dial_code'])?$sim_allocation['dial_code']:null;
+                $sim->phone_number = isset($sim_allocation['phone_number'])?$sim_allocation['phone_number']:null;
+                $sim->agent_id = $agent->id;
+                $sim->save();
+            }
+        }
+        $this->response['success'] = 1;
+        return response()->json($this->response);
     }
     public function registerAgent(Request $request) {
         $this->validate($request, [
@@ -73,39 +100,49 @@ class CallRecorderController extends Controller {
     }
     public function createLog(Request $request) {
         $this->validate($request, [
-            'api_token' => 'required|string|size:40|exists:agents',
             'sim_id' => 'required|digits_between:10,50|exists:sim_allocations,id',
             'dial_code' => 'required|string|max:5',
             'phone_number' => 'required|string|max:15',
             'saved_name' => 'nullable|string|max:100',
             'duration' => 'required|integer|min:0',
             'device_time' => 'required|date_format:Y-m-d H:i:s',
-            'call_type' => 'required|in:incoming,outgoing',
-            'status' => 'required|boolean'
+            'call_type' => 'required|in:incoming,outgoing,missed',
         ]);
-        $agent = Agents::where('api_token', $request->api_token)->first();
-        $callRegister = new CallRegister;
-        $callRegister->sim_allocation_id = $request->sim_id;
-        $callRegister->agent_id = $agent->id;
-        $callRegister->dial_code = $request->dial_code;
-        $callRegister->phone_number = $request->phone_number;
-        $callRegister->saved_name = $request->saved_name;
-        $callRegister->duration = $request->duration;
-        $callRegister->device_time = $request->device_time;
-        $callRegister->call_type = $request->call_type;
-        $callRegister->status = $request->status;
-        $callRegister->save();
-        $this->response['message'] = 'success';
-        return response()->json($this->response);
+        $sim = SimAllocation::find($request->sim_id);
+        if($sim) {
+            $callRegister = new CallRegister;
+            $callRegister->sim_allocation_id = $request->sim_id;
+            $callRegister->agent_id = $sim->agent_id;
+            $callRegister->dial_code = $request->dial_code;
+            $callRegister->phone_number = $request->phone_number;
+            $callRegister->saved_name = $request->saved_name;
+            $callRegister->duration = $request->duration;
+            $callRegister->device_time = $request->device_time;
+            if($request->call_type == 'incoming') {
+                $callRegister->call_type = $request->call_type;
+                $callRegister->status = (intval($callRegister->duration))?1:0;
+            } elseif($request->call_type == 'outgoing') {
+                $callRegister->call_type = $request->call_type;
+                $callRegister->status = (intval($callRegister->duration))?1:0;
+            } elseif($request->call_type == 'missed') {
+                $callRegister->call_type = 'incoming';
+                $callRegister->status = 0;
+            }
+            $callRegister->save();
+            $this->response['success'] = 1;
+            return response()->json($this->response);
+        } else {
+            $this->response['success'] = 0;
+            return response()->json($this->response, 422);
+        }
     }
     public function displayLog(Request $request) {
         $this->validate($request, [
             'date' => 'sometimes|required|date_format:Y-m-d',
-            'agent_id' => 'required|exists:agents,id'
+            'user_name' => 'required|exists:agents'
         ]);
-        $agent = Agents::find($request->agent_id);
+        $agent = Agents::where('user_name', $request->user_name)->first();
         $logs = CallRegister::where('agent_id', $agent->id)->whereDate('device_time', $request->input('date', date('Y-m-d')))->orderBy('device_time', 'desc')->get();
-        $this->response['message'] = 'success';
         $this->response['logs'] = [];
         foreach($logs as $log) {
             $this->response['logs'][] = [
@@ -114,9 +151,8 @@ class CallRecorderController extends Controller {
                 'phone_number' => $log->phone_number,
                 'saved_name' => $log->saved_name,
                 'duration' => $log->duration,
-                'timestamp' => $log->device_time,
-                'call_type' => $log->call_type,
-                'status' => $log->status
+                'timestamp' => date($log->device_time, 'Y-m-d H:i:s'),
+                'call_type' => ($log->call_type=='incoming'&&$log->status==0)?'missed':$log->call_type
             ];
         }
         return response()->json($this->response);
