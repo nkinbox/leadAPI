879c97e1ce415c64ba1f6246825432111e69eb65
unknown <nkinbox@outlook.com>
2019-11-11 00:06:35 +0530

    Logout

--- resources/views/call_log.blade.php
+++ resources/views/call_log.blade.php
@@ -29,23 +29,15 @@
                     </li>
                 </ul>
                 <search></search>
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" tabindex="-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
+                <a class="dropdown-item" tabindex="-1" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
 
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" tabindex="-1" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{csrf_field()}}
-                        </form>
-                    </div>
-                </li>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{csrf_field()}}
+                </form>
             </div>
         </nav>
         <router-view style="margin-top: 66px"></router-view>
