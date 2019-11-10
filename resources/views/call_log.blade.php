cc9a00c6430812843e04207b2eaca2ccffdc5825
unknown <nkinbox@outlook.com>
2019-11-11 00:04:31 +0530

    Logout

--- resources/views/call_log.blade.php
+++ resources/views/call_log.blade.php
@@ -29,6 +29,23 @@
                     </li>
                 </ul>
                 <search></search>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" tabindex="-1" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" tabindex="-1" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{csrf_field()}}
+                        </form>
+                    </div>
+                </li>
             </div>
         </nav>
         <router-view style="margin-top: 66px"></router-view>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}" tabindex="-1">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="https://www.tripclues.in/leadAPI/public/vuejs/logo.png" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
