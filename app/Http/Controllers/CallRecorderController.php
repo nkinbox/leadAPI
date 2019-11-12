0065692f50c2d0ded834b9d5efc482909cea9c03
unknown <nkinbox@outlook.com>
2019-11-12 21:30:25 +0530

    Call Calculation

--- app/Http/Controllers/CallRecorderController.php
+++ app/Http/Controllers/CallRecorderController.php
@@ -257,11 +257,13 @@ class CallRecorderController extends Controller {
                 'unique' => []
             ]
         ];
-        DB::statement('create temporary table temp_call_logs(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, is_unique INT DEFAULT 0) '.$logs->toSql(), $logs->getBindings());
-        DB::statement('create temporary table temp_unique_calls select min(id) as id, group_concat(distinct call_type), count(id) as count_number from temp_call_logs group by concat(dial_code, phone_number, call_type)');
+        DB::statement('create temporary table temp_call_logs(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, is_unique INT DEFAULT 0, is_latest INT DEFAULT 0) '.$logs->toSql(), $logs->getBindings());
+        DB::statement('create temporary table temp_unique_calls select min(id) as id from temp_call_logs group by concat(dial_code, phone_number, call_type)');
         DB::update('update temp_call_logs inner join temp_unique_calls on temp_unique_calls.id = temp_call_logs.id set is_unique = 1');
-
-        dump(DB::table('temp_unique_calls')->get());
+        DB::statement('delete from temp_unique_calls');
+        DB::insert('insert into temp_unique_calls select min(id) as id from temp_call_logs group by concat(dial_code, phone_number)');
+        DB::update('update temp_call_logs inner join temp_unique_calls on temp_unique_calls.id = temp_call_logs.id set is_latest = 1');
+        DB::statement('drop temporary table temp_unique_calls');
         dd(DB::table('temp_call_logs')->get());
 
 
