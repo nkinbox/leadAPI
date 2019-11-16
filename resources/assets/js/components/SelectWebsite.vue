f230dc708d532bd1add3e26f67be36dac88e0237
unknown <nkinbox@outlook.com>
2019-11-16 10:57:54 +0530

    Push To Crm

--- resources/assets/js/components/SelectWebsite.vue
+++ resources/assets/js/components/SelectWebsite.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="d-flex">
-        <select v-model="selectWebsite" class="form-control flex-fill">
-            <option v-for="website in websites" :key="website.id" :value="website.id">{{website.display_name}}</option>
-        </select>
-        <button class="btn btn-primary" @click="refresh">&#8634;</button>
+    <div>
+        <div class="d-flex">
+            <select v-model="selectWebsite" class="form-control flex-fill">
+                <option v-for="website in websites" :key="website.id" :value="website.id">{{website.display_name}}</option>
+            </select>
+            <button class="btn btn-primary" @click="refresh">&#8634;</button>
+        </div>
     </div>
 </template>
 
